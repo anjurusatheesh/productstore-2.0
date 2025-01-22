718126e226f7f63e30ddd1067ca1f63a0bed6712
anjurusatheesh <anjurusatheesh@gmail.com>
2025-01-22 12:10:11 +0530

    Update PurchaseController.java

--- Back-End/src/main/java/Eminds/ProductStore/controller/PurchaseController.java
+++ Back-End/src/main/java/Eminds/ProductStore/controller/PurchaseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://192.168.190.131:3000")
 @RestController
 @RequestMapping("/api/v2")
 public class PurchaseController {
