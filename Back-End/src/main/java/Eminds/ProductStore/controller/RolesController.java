cb1de7faa08e2dc958402a2bf05d415f8489cc0c
anjurusatheesh <anjurusatheesh@gmail.com>
2025-01-22 12:10:41 +0530

    Update RolesController.java

--- Back-End/src/main/java/Eminds/ProductStore/controller/RolesController.java
+++ Back-End/src/main/java/Eminds/ProductStore/controller/RolesController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://192.168.190.131:3000")
 @RestController
 @RequestMapping("/api/v5")
 public class RolesController {
